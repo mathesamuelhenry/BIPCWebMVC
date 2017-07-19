194f98d3804f11b92133d91f6e1266f9a25837f6
Mathe, Samuel <samuel.mathe@lexisnexisrisk.com>
2017-07-19 15:35:50 -0400

    Initial project checkin

---
+++ BIPCMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BIPCMVC.Startup))]
+namespace BIPCMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
