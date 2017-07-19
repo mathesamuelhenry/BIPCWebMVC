06f844dc82e125a06afdeeb2e2906d74b67580c5
Mathe, Samuel <samuel.mathe@lexisnexisrisk.com>
2017-07-19 15:55:44 -0400

    #1 adding bootstrap templates

--- BIPCMVC/App_Start/BundleConfig.cs
+++ BIPCMVC/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace BIPCMVC
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-spacelab.css",
                       "~/Content/site.css"));
         }
     }
